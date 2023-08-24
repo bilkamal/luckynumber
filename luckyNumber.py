8ed26a7cdf663f4bc41c41a791f28eaa48cb3234
bil <71746697+bilkamal@users.noreply.github.com>
2023-08-24 19:23:21 +0700

    Update luckyNumber.py
    Fix bug

--- luckyNumber.py
+++ luckyNumber.py
@@ -1,7 +1,8 @@
-name = input("Please insert your name: ")
-
 def luckyNumber(name):
     number = len(name)*9
     print("Hello ", name,". Your lucky number is: ",str(number))
-  
+
+
+name = input("Please insert your name: ")
+luckyNumber(name)
 input("Thanks for using this program, Press anyting for exit...")
