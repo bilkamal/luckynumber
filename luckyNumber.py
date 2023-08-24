9309e089931f200ef14ae0d9ae067bb63ff43d68
bil <71746697+bilkamal@users.noreply.github.com>
2023-08-24 19:20:09 +0700

    Revert "Lucky Number V1.1"

--- luckyNumber.py
+++ luckyNumber.py
@@ -1,7 +1,4 @@
-name = input("Please insert your name: ")
-
 def luckyNumber(name):
     number = len(name)*9
     print("Hello ", name,". Your lucky number is: ",str(number))
   
-input("Thanks for using this program, Press anyting for exit...")
